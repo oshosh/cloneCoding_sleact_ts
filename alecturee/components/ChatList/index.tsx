4885ee84b5d6bd048bef2f02764525585ae4d127
osh <dhtpgus7@gmail.com>
2022-02-08 23:13:14 +0900

    다이렉트 메세지 (ChatBox, ChatList) 퍼블리싱

---
+++ alecturee/components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChatZone } from '@components/ChatList/styles';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <section>section</section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
--- alecturee/pages/DirectMessage/index.tsx
+++ alecturee/pages/DirectMessage/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
 
-function DirectMessage() {
-  return <>d</>;
-}
+const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname}></img>
+      </Header>
+      {/* 채팅 현황 */}
+      <ChatList />
+      {/* 채팅 박스 */}
+      <ChatBox chat="" />
+    </Container>
+  );
+};
 
 export default DirectMessage;
